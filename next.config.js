ed25ac0464b37a318a498591041c44fb8a82dd8a
Albert <niksers@mail.ru>
2023-05-17 18:52:49 +0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  basePath: "/test",
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+      },
+    ];
+  },
+};
